cd46ee17d67c76ac1ae3a26883e9840ff254448b
Danilo Mendonça <danilomendonca08@gmail.com>
2024-09-22 22:17:09 -0300

    added bean validation to the User class

--- src/main/java/br/com/danilo/gamintory/user/User.java
+++ src/main/java/br/com/danilo/gamintory/user/User.java
@@ -2,6 +2,7 @@ package br.com.danilo.gamintory.user;
 
 import br.com.danilo.gamintory.game.Game;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -14,9 +15,17 @@ public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @NotBlank
     String name;
+
+    @NotBlank
     String username;
+
+    @NotBlank
     String password;
+
+
     LocalDateTime createdAt;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)

8cd112ae26c0695b9473ef8583ab3d3555cf8160
Danilo Mendonça <danilomendonca08@gmail.com>
2024-09-24 16:53:38 -0300

    Created TokenService with methods to create a Token and a method to get a user from his Token

---
+++ src/main/java/br/com/danilo/gamintory/auth/TokenService.java
@@ -0,0 +1,48 @@
+package br.com.danilo.gamintory.auth;
+
+import br.com.danilo.gamintory.user.User;
+import br.com.danilo.gamintory.user.UserRepository;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    private final UserRepository userRepository;
+    private Algorithm algorithm;
+
+    public TokenService(UserRepository userRepository, @Value("${jwt.secret}") String secret) {
+        this.userRepository = userRepository;
+        algorithm = Algorithm.HMAC256(secret);
+    }
+
+    public Token create(User user) {
+
+        var expiresAt = LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.ofHours(-3));
+        String token = JWT.create()
+                .withIssuer("gamintory")
+                .withSubject(user.getUsername())
+                .withClaim("role", "admin")
+                .withExpiresAt(expiresAt)
+                .sign(algorithm);
+
+        return new Token(token);
+    }
+
+    public User getUserFromToken(String token) {
+        var username = JWT.require(algorithm)
+                .withIssuer("gamintory")
+                .build()
+                .verify(token)
+                .getSubject();
+
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}

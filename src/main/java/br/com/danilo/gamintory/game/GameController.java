ef22fb5f78cc2f7de1a7ed24dbf6b51d11333001
Danilo Mendonça <danilomendonca08@gmail.com>
2024-09-24 22:37:19 -0300

    changed findAll method to return a pageable and modifies URL of the other endpoints

--- src/main/java/br/com/danilo/gamintory/game/GameController.java
+++ src/main/java/br/com/danilo/gamintory/game/GameController.java
@@ -1,12 +1,15 @@
 package br.com.danilo.gamintory.game;
 
+import br.com.danilo.gamintory.auth.TokenService;
+import br.com.danilo.gamintory.user.UserService;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+
 
 import java.util.List;
 
@@ -17,12 +20,20 @@ public class GameController {
     @Autowired
     GameService service;
 
-    public ResponseEntity<Game> add(Game game, UriComponentsBuilder uriBuilder) {
+    private final UserService userService;
+
+    public GameController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Game> create(@RequestBody Game game, UriComponentsBuilder uriBuilder) {
+
         service.add(game);
 
         var uri = uriBuilder
                 .path("/games/{id}")
-                .buildAndExpand(game.getId())
+                .buildAndExpand(game.getGameId())
                 .toUri();
 
         return ResponseEntity
@@ -31,26 +42,26 @@ public class GameController {
     }
 
     @GetMapping
-    public List<Game> findAll() {
-        return service.findAll();
+    public Page<Game> findAll(@PageableDefault(size = 3)Pageable pageable) {
+        return service.findAll(pageable);
     }
 
-    @GetMapping
+    @GetMapping("/name")
     public List<Game> findByName(@RequestParam String name) {
         return service.findByName(name);
     }
 
-    @GetMapping
+    @GetMapping("/genre")
     public List<Game> findByGenre(@RequestParam String genre) {
         return service.findByGenre(genre);
     }
 
-    @GetMapping
+    @GetMapping("/stars")
     public List<Game> findByStars(@RequestParam Integer stars) {
         return service.findByStars(stars);
     }
 
-    @GetMapping
+    @GetMapping("/year")
     public List<Game> findByYear(@RequestParam Integer year) {
         return service.findByYear(year);
     }
